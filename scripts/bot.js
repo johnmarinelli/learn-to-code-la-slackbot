ef4a3f50fb807c9d78aca795b4eecd0b99675ea0
john marinelli <jonmarinelli@hotmail.com>
2015-11-24 20:54:15 -0800

    lulz

--- scripts/bot.js
+++ scripts/bot.js
@@ -1,8 +1,7 @@
 "use strict"
 
-module.exports=function(robot){
+module.exports = function(robot){
   robot.respond(/animate me (.+)/i, function(msg){
-  msg.send("No.");
-  };
-
+    msg.send("No.");
+  });
 }
