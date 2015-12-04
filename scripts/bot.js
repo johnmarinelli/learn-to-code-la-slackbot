ed9a40e44d6cbd4db54b7e938e1aa2dad31578a5
john marinelli <jonmarinelli@hotmail.com>
2015-12-04 15:17:15 -0800

    add sandbox

--- scripts/bot.js
+++ scripts/bot.js
@@ -1,6 +1,12 @@
 "use strict"
+var sandbox = require('sandbox');
 
 module.exports = function(robot){
-  /*robot.respond(/(.+)/i, function(msg){
-  });*/
+  robot.respond(/call (.+)/i, function(msg) {
+    var code = msg.match[1];
+    var s = new sandbox();
+    s.run(code, function(out) {
+      msg.send(out.result);
+    });
+  });
 }
